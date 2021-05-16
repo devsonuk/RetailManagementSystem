10ffcbf6647d3c46a533551c1bb8da2c9c3d67d8
Sonu Kumar <mailtosonukmr@gmail.com>
2021-05-16 15:55:22 +0530

    Register DB Tables
    The tables for the basic register system.

---
+++ RMSData/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+    [Id] NCHAR(128) NOT NULL  PRIMARY KEY,
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getUTCDate(),
+
+)
